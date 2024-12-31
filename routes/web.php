cd6a90a736db49615e695ecd41bb8d5f3b095246
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-31 12:19:29 -0300

    Criando Rota de Usuarios

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// routes/web.php (Projeto 1)
+use App\Http\Controllers\UsuarioController;
+
+// Rota para visualizar todos os usuários
+Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');

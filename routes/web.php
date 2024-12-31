7850a094fed8bbf27398a70cbd1d7e135034b3b9
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-31 12:20:18 -0300

    Criando Rotas de Upload

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 // routes/web.php (Projeto 1)
 use App\Http\Controllers\UsuarioController;
+use App\Http\Controllers\UploadController;
 
 // Rota para visualizar todos os usuários
 Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index');
+
+// Rota para o formulário de upload
+Route::get('/upload', [UploadController::class, 'index'])->name('upload.index');
+Route::post('/upload', [UploadController::class, 'uploadArquivo'])->name('upload.arquivo');
